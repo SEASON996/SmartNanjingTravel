feceeb8f4b362d89617e170a5ef23732161e7355
SEASON996 <3143808621@qq.com>
2026-01-07 10:43:01 +0800

    添加收藏

---
+++ Models/FavoriteItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartNanjingTravel.Models
+{
+    /// <summary>
+    /// 收藏项数据模型
+    /// </summary>
+    public class FavoriteItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }  // "景点", "路线"
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime AddedDate { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string Location { get; set; } // 详细地址
+    }
+}
