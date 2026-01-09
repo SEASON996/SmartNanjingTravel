366a12a8b7bc03eccb23cee514b72dfc7e708959
yu hou <hutupp9921@163.com>
2026-01-09 16:04:01 +0800

    卫星地图

---
+++ ViewModels/LayerItem.cs
@@ -0,0 +1,33 @@
+﻿// ViewModels/LayerItem.cs
+using Esri.ArcGISRuntime.Mapping;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace SmartNanjingTravel.ViewModels
+{
+    public class LayerItem : INotifyPropertyChanged
+    {
+        public string Name { get; set; }
+        public Layer Layer { get; set; }
+
+        private bool _isVisible;
+        public bool IsVisible
+        {
+            get => _isVisible;
+            set
+            {
+                if (_isVisible != value)
+                {
+                    _isVisible = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
